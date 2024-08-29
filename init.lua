c7fbdc0ac585500786e76037e070821a9dc8e214
jramirez <justin.ramirez@gametime.co>
2024-08-29 01:35:05 -0400

    add avante

--- init.lua
+++ init.lua
@@ -136,6 +136,45 @@ require("lazy").setup({
     -- Indent Blankline plugin
    -- { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
+    -- avante: Similar to cursor ai but for neovim
+    {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    opts = {
+        -- add any opts here
+    },
+    dependencies = {
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        --- The below dependencies are optional,
+        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- recommended settings
+            default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+                insert_mode = true,
+            },
+            },
+        },
+        },
+        {
+        -- Make sure to setup it properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+            file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+        },
+    },
+    },
+
     -- LSP Plugins
     {
         "williamboman/mason.nvim",
