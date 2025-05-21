a43caf42dd5a7cc65dfb40044efbc29d7032b586
jramirez <justin.ramirez@gametime.co>
2025-05-21 10:18:14 -0400

    update config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,7 +14,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 		os.exit(1)
 	end
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.runtimepath:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
