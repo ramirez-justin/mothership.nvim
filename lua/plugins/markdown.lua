c666b7fed65298ef336699b0f65dd5fd34240b5b
jram <95737913+ramirez-justin@users.noreply.github.com>
2026-04-02 22:48:50 -0400

    fix: remove neorg

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -10,10 +10,4 @@ return {
             vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
         end,
     },
-    {
-        "nvim-neorg/neorg",
-        lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-        version = "*", -- Pin Neorg to the latest stable release
-        config = true,
-    },
 }
